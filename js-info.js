0c93c1bd6eb6aa5336947fad0d1277886ee93330
Teen <136805513+HregTeen@users.noreply.github.com>
2023-06-17 16:29:52 +0300

    Add files via upload

---
+++ js-info.js
@@ -0,0 +1,5 @@
+prompt("Кто разработал язык программирования Java Script?","Фамилия Имя");
+prompt("За сколько дней был разработан Java Script?","");
+prompt("В каком году появился Java Sript?","");
+prompt("Java Sript это тоже самое,что Java?","Отвечайте либо Java Script это Java, либо Java Script это не Java");
+prompt("Как навзывается спецификация Java Script?");
